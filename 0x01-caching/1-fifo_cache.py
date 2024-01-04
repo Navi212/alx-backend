5aeb63a774e3dd64507fb691e5b8ce770af56730
Joseph Nweke <josephchristo615@gmail.com>
2024-01-04 03:07:19 +0100

    Adds initialization for put method

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -42,6 +42,7 @@ class FIFOCache(BaseCaching):
             del_key = next(iter(self.cache_data))
             del self.cache_data[del_key]
             print(f"DISCARD: {del_key}")
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Returns the item associated with a key in cache_data """
